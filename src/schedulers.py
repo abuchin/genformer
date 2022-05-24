eb8d70c476d0e2b6cd77f38d721c83a2cd08fea1
njaved <njaved@mit.edu>
2022-05-24 19:08:01 +0000

    added cosine decay scheduler

--- src/schedulers.py
+++ src/schedulers.py
@@ -7,23 +7,63 @@ import tensorflow as tf
 
 from tensorflow.keras import layers as kl
 
+@tf.keras.utils.register_keras_serializable()
+class cosine_decay_w_warmup(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """Cosine decay schedule with warm up period.
+    mod from https://github.com/Tony607/Keras_Bag_of_Tricks/blob/master/warmup_cosine_decay_scheduler.py
+    """
+    def __init__(self,
+                 peak_lr,
+                 initial_learning_rate,
+                 total_steps,
+                 warmup_steps,
+                 hold_peak_rate_steps,
+                 name = None):
+        self.peak_lr = peak_lr
+        self.initial_learning_rate = initial_learning_rate
+        self.total_steps = total_steps
+        self.warmup_steps = warmup_steps
+        self.hold_peak_rate_steps = hold_peak_rate_steps
+        self.name = name
 
-
-def cosine_decay(global_step,
-                 decay_steps,
-                 min_learning_rate):
-    ## copied from the TF implementation in function form
-    step = min(step, decay_steps)
-    
-    cosine_decay = 0.5 * (1 + cos(pi * step / decay_steps))
-
-    decayed = (1 - alpha) * cosine_decay + alpha
-    return initial_learning_rate * decayed
-
-'''
-Genformer learning rate schedulers
-'''
-
+    def __call__(self, step):
+        with tf.name_scope(self.name or "cosine_decay_w_warmup"):
+            initial_learning_rate = tf.convert_to_tensor(
+                self.initial_learning_rate, name = "initial_learning_rate")
+        dtype = initial_learning_rate.dtype
+        peak_lr = tf.cast(self.peak_lr, dtype)
+        total_steps = tf.cast(self.total_steps,dtype)
+        warmup_steps = tf.cast(self.warmup_steps,dtype)
+        hold_peak_rate_steps = tf.cast(self.hold_peak_rate_steps,dtype)
+        
+        global_step_recomp = tf.cast(step, dtype)
+        
+        learning_rate = 0.5 * peak_lr * (1. + tnp.cos(
+            tnp.pi * (global_step_recomp - warmup_steps - hold_peak_rate_steps
+                    ) / (total_steps - warmup_steps - hold_peak_rate_steps)))
+                                         
+        learning_rate = tf.cond(tf.math.greater(hold_peak_rate_steps,0),
+                                lambda: tnp.where(global_step_recomp > warmup_steps + hold_peak_rate_steps,
+                                     learning_rate, peak_lr),
+                                lambda: learning_rate)
+                                
+        slope = (initial_learning_rate - peak_lr) / warmup_steps
+        warmup_rate = slope * global_step_recomp + initial_learning_rate
+        learning_rate = tf.cond(tf.math.greater(warmup_steps, 0),
+                                lambda: tnp.where(global_step_recomp < warmup_steps, warmup_rate,
+                                         learning_rate),
+                                lambda: learning_rate)
+        return tnp.where(global_step_recomp > total_steps, initial_learning_rate, learning_rate)
+                                         
+    def get_config(self):
+        return {
+            "peak_lr": self.peak_lr,
+            "initial_learning_rate": self.initial_learning_rate,
+            "total_steps": self.total_steps,
+            "warmup_steps": self.warmup_steps,
+            "hold_peak_rate_steps": self.hold_peak_rate_steps,
+            "name": self.name
+        }
 
 '''
 cosine decay w/ warmup 
@@ -31,7 +71,7 @@ taken from
 https://github.com/Tony607/Keras_Bag_of_Tricks/blob/master/warmup_cosine_decay_scheduler.py
 '''
 #@tf.keras.utils.register_keras_serializable
-def cosine_decay_with_warmup(global_step,
+def cosine_decay_with_warmup_func(global_step,
                              learning_rate_base,
                              total_steps,
                              warmup_learning_rate=0.0,
