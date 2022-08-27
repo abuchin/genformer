7004d274fb9a67ba5da12ed3f4c84d80949671b7
Nauman Javed <javed@javed.local>
2022-08-27 08:39:36 -0400

    mod execute sweep order of inputs

--- execute_sweep_5.sh
+++ execute_sweep_5.sh
@@ -10,17 +10,17 @@ python3 train_model_aformer_TF_genecentered_separated.py \
             --gcs_path="gs://picard-testing-176520/16k_genecentered_blacklist0.50_atacnormalized/preprocessed" \
             --gcs_path_val_ho="gs://picard-testing-176520/16k_genecentered_blacklist0.50_atacnormalized/val_holdout/preprocessed/val" \
             --output_heads="hg" \
-            --input_length="16384,31072,65536,131072,196608" \
+            --input_length="16384,196608,131072,65536" \
             --max_shift=300 \
             --target_unit="logTPM" \
             --batch_size=72 \
-            --num_epochs=50 \
+            --num_epochs=25 \
             --train_steps=298 \
-            --warmup_frac=0.05 \
+            --warmup_frac=0.03 \
             --total_steps=14900 \
             --val_steps_h=47 \
             --val_steps_ho=6 \
-            --patience=15 \
+            --patience=7 \
             --min_delta=0.001 \
             --model_save_dir="gs://picard-testing-176520/16k_genecentered_blacklist0.50_atacnormalized/models" \
             --model_save_basename="aformer_initial_tests" \
@@ -49,7 +49,7 @@ python3 train_model_aformer_TF_genecentered_separated.py \
             --rel_pos_bins=512 \
             --kernel_transformation="relu_kernel_transformation" \
             --kernel_regularizer="0" \
-            --savefreq=10 \
+            --savefreq=8 \
             --use_rot_emb="True" \
             --use_mask_pos="False" \
             --use_fft_prior="True" \
@@ -57,4 +57,4 @@ python3 train_model_aformer_TF_genecentered_separated.py \
             --fft_prior_scale="0.20" \
             --bottleneck_units="32" \
             --bottleneck_units_tf="32" \
-            --use_tf_acc="False,True"
+            --use_tf_acc="False"
