c50e8f8c5c718ef79073f8c253ed7fec1f1a873c
njaved <njaved@mit.edu>
2022-09-27 03:17:15 +0000

    remove enformer vanilla references, add scipy pip install

--- tpu_pod_init_script.sh
+++ tpu_pod_init_script.sh
@@ -7,10 +7,11 @@ sudo pip3 install pandas
 sudo pip3 install seaborn
 sudo pip3 install einops
 sudo pip3 install tqdm
+sudo pip3 install scipy
 sudo pip3 install wandb
 sudo pip3 install plotly
 sudo pip3 install tensorboard-plugin-profile==2.4.0
-export TPU_NAME=node-24
+export TPU_NAME=pod
 export ZONE=us-east1-d 
 export TPU_LOAD_LIBRARY=0
 
