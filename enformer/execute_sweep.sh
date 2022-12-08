0841d9c943bb52e3b0ff475e0a2183a0e4d75415
njaved <njaved@mit.edu>
2022-12-08 03:42:26 +0000

    change default tpu

--- enformer/execute_sweep.sh
+++ enformer/execute_sweep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 python3 train_model.py \
-            --tpu_name="node-4" \
-            --tpu_zone="us-central1-a" \
+            --tpu_name="pod" \
+            --tpu_zone="us-east1-d" \
             --wandb_project="enformer_baseline" \
             --wandb_user="njaved" \
             --wandb_sweep_name="enformer_baseline" \
