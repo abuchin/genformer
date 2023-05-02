9038d925d84d7b117c21e762088817f437e3760a
njaved <njaved@mit.edu>
2023-05-02 03:58:56 +0000

    change default data path to 196k

--- .ipynb_checkpoints/execute_sweep_atac-checkpoint.sh
+++ .ipynb_checkpoints/execute_sweep_atac-checkpoint.sh
@@ -7,11 +7,11 @@ python3 train_model_atac.py \
             --wandb_user="njaved" \
             --wandb_sweep_name="atac_pretraining" \
             --gcs_project="picard-testing-176520" \
-            --gcs_path="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_human" \
-            --gcs_path_mm="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_mouse" \
-            --gcs_path_rm="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_rhesus" \
-            --gcs_path_rat="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_rat" \
-            --gcs_path_holdout="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_val_holdout" \
+            --gcs_path="gs://picard-testing-176520/genformer_atac_pretrain/196k/genformer_atac_pretrain_globalacc_conv_rpgc_human" \
+            --gcs_path_mm="gs://picard-testing-176520/genformer_atac_pretrain/196k/genformer_atac_pretrain_globalacc_conv_rpgc_mouse" \
+            --gcs_path_rm="gs://picard-testing-176520/genformer_atac_pretrain/196k/genformer_atac_pretrain_globalacc_conv_rpgc_rhesus" \
+            --gcs_path_rat="gs://picard-testing-176520/genformer_atac_pretrain/196k/genformer_atac_pretrain_globalacc_conv_rpgc_rat" \
+            --gcs_path_holdout="gs://picard-testing-176520/genformer_atac_pretrain/196k/genformer_atac_pretrain_globalacc_conv_rpgc_val_holdout" \
             --input_length=196608 \
             --output_length=1536 \
             --output_length_ATAC=49152 \
