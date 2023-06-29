59bbeab352e17e019f6cef0a423576e2bc25651d
njaved <njaved@mit.edu>
2023-06-29 01:35:35 +0000

    resolving val holdout path

--- execute_sweep_atac.sh
+++ execute_sweep_atac.sh
@@ -11,7 +11,7 @@ python3 train_model_atac.py \
             --gcs_path_mm="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_mouse" \
             --gcs_path_rm="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_rhesus" \
             --gcs_path_rat="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_rat" \
-            --gcs_path_holdout="gs://picard-testing-176520/genformer_atac_pretrain/524/genformer_atac_pretrain_globalacc_conv_rpgc_val_holdout" \
+            --gcs_path_holdout="gs://picard-testing-176520/genformer_atac_pretrain/262k/genformer_atac_pretrain_globalacc_conv_rpgc_val_holdout" \
             --training_type="hg_mm_rm_rat" \
             --input_length=262144 \
             --output_length=2048 \
