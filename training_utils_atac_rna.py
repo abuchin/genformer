5c27d9b098b92033dfe860deada9c114fedc847f
njaved <njaved@mit.edu>
2023-10-09 15:01:36 -0400

    remove exponentiation of rna out vector, since now data writing script already does that transformation

--- training_utils_atac_rna.py
+++ training_utils_atac_rna.py
@@ -485,9 +485,9 @@ def deserialize_val(serialized_example, g, use_tf_activity, input_length = 19660
                                                stddev=0.001,
                                                dtype=tf.float32))
     '''scale'''
-    percentile99 = (tfp.stats.percentile(tf_activity, q=99.0, axis=1) + 1.0e-04)
+    #percentile99 = (tfp.stats.percentile(tf_activity, q=99.0, axis=1) + 1.0e-04)
     #
-    tf_activity = tf_activity / percentile99
+    #tf_activity = tf_activity / percentile99
 
     peaks_sum = tf.reduce_sum(peaks_center)
     seq_seed = tf.reduce_sum(sequence[:,0])
