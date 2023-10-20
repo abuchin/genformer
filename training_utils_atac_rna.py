2b5285ad820f0ebd99d4c5a493a2faaf893ba34a
njaved <njaved@mit.edu>
2023-10-19 22:44:54 -0400

    switch to atac loss scaling. also rescale rna target data, try raising to 3/4 power

--- training_utils_atac_rna.py
+++ training_utils_atac_rna.py
@@ -402,8 +402,8 @@ def deserialize_tr(serialized_example, g, use_tf_activity, input_length = 196608
                         [crop_size,0],
                         [output_length-2*crop_size,-1])
     rna_out = tf.math.pow(rna_out * weighting_factor,0.75)
-    diff = tf.math.sqrt(tf.nn.relu(rna_out - 10000.0 * tf.ones(rna_out.shape)))
-    rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=10000.0) + diff
+    diff = tf.math.sqrt(tf.nn.relu(rna_out - 2500.0 * tf.ones(rna_out.shape)))
+    rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=2500.0) + diff
 
     peaks_gathered = tf.reduce_max(tf.reshape(peaks_crop, [(output_length-2*crop_size) // 4, -1]),
                                    axis=1,keepdims=True)
@@ -576,8 +576,8 @@ def deserialize_val(serialized_example, g, use_tf_activity, input_length = 19660
                         [crop_size,0],
                         [output_length-2*crop_size,-1])
     rna_out = tf.math.pow(rna_out * weighting_factor,0.75)
-    diff = tf.math.sqrt(tf.nn.relu(rna_out - 10000.0 * tf.ones(rna_out.shape)))
-    rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=10000.0) + diff
+    diff = tf.math.sqrt(tf.nn.relu(rna_out - 2500.0 * tf.ones(rna_out.shape)))
+    rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=2500.0) + diff
 
     peaks_gathered = tf.reduce_max(tf.reshape(peaks_crop, [(output_length-2*crop_size) // 4, -1]),
                                    axis=1,keepdims=True)
