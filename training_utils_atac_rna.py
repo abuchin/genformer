27420acea236ac29704c931f5737a53f6f515cc0
njaved <njaved@mit.edu>
2023-10-24 20:27:43 -0400

    take sqrt root instead of raising to 0.75 power

--- training_utils_atac_rna.py
+++ training_utils_atac_rna.py
@@ -398,7 +398,7 @@ def deserialize_tr(serialized_example, g, use_tf_activity, input_length = 196608
     rna_out = tf.slice(rna,
                         [crop_size,0],
                         [output_length-2*crop_size,-1])
-    rna_out = tf.math.pow(rna_out * weighting_factor,0.75)
+    rna_out = tf.math.pow(rna_out * weighting_factor,0.50)
     diff = tf.math.sqrt(tf.nn.relu(rna_out - 2500.0 * tf.ones(rna_out.shape)))
     rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=2500.0) + diff
 
@@ -572,7 +572,7 @@ def deserialize_val(serialized_example, g, use_tf_activity, input_length = 19660
     rna_out = tf.slice(rna,
                         [crop_size,0],
                         [output_length-2*crop_size,-1])
-    rna_out = tf.math.pow(rna_out * weighting_factor,0.75)
+    rna_out = tf.math.pow(rna_out * weighting_factor,0.50)
     diff = tf.math.sqrt(tf.nn.relu(rna_out - 2500.0 * tf.ones(rna_out.shape)))
     rna_out = tf.clip_by_value(rna_out, clip_value_min=0.0, clip_value_max=2500.0) + diff
 
